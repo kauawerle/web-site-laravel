72d77504ef7bf21732421e8e5d53c8bd46588fa8
Kauã Werle <kauawerle19@gmail.com>
2025-05-12 10:24:21 -0300

    Create Model Job

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,48 +11,14 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '50.000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '10.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '40.000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/job/{id}', function ($id) {
-   $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '50.000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '10.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '40.000'
-            ]
-        ];
+    $job = Job::find( $id );
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
-    return view('job', ['job' => $job]);
+    return view('job', ['job' => $job ]);
 });
 
 Route::get('/contact', function () {
